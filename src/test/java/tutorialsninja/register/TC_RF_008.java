69d803cb3c4c0b179aa95457ecfc9be60fb1d783
darshanjoshi391 <darshanjoshi391@gmail.com>
2025-10-20 17:57:41 +0530

    TC_RF_008.java

---
+++ src/test/java/tutorialsninja/register/TC_RF_008.java
@@ -0,0 +1,56 @@
+package tutorialsninja.register;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.CommonUtils;
+import java.time.Duration;
+
+public class TC_RF_008
+{
+    WebDriver driver;
+
+    @Test (dataProvider = "PasswordSupplier")
+    public void VerifyRegisterAccPass (String PasswordText)
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+        driver.get("https://tutorialsninja.com/demo/");
+
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("Darshan");
+        driver.findElement(By.id("input-lastname")).sendKeys("Joshi");
+        driver.findElement(By.id("input-email")).sendKeys(CommonUtils.GenerateEmail());
+        driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+        driver.findElement(By.id("input-password")).sendKeys(PasswordText);
+        driver.findElement(By.id("input-confirm")).sendKeys(PasswordText);
+        driver.findElement(By.xpath("//input[@name='newsletter'][@value=1]")).click();
+        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+        String WarningMessage = "Password entered is not matching the complexity standards";
+        Assert.assertFalse(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Success']")).isDisplayed());
+
+    }
+
+    @DataProvider (name="PasswordSupplier")
+    public Object[][] SetofPass ()
+    {
+        Object[][] data = {{"12345"}, {"abcdef"}, {"abcd12345"}, {"acbd123$"}, {"ABCD123#"}};
+        return data;
+
+    }
+
+    @AfterMethod
+    public void teardown ()
+    {
+        driver.quit();
+    }
+
+}
